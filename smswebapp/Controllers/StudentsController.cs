792001d8cdae4ecf59081b7adb7fe73174a5f127
akshit0009 <152368278+akshit0009@users.noreply.github.com>
2024-09-10 12:18:57 +0530

    Update StudentsController.cs

--- smswebapp/Controllers/StudentsController.cs
+++ smswebapp/Controllers/StudentsController.cs
@@ -6,7 +6,8 @@ public class StudentsController : Controller{
 
     private static List<Student> students = new List<Student>{
         new Student {Id=1, Name="Dharmesh", Address="Delhi"},
-        new Student {Id=2, Name="Ramesh", Address="Surat"}
+        new Student {Id=2, Name="Ramesh", Address="Surat"},
+        new Student {Id=3, Name="Akshit", Address="Hyd"}
     };
 
         public IActionResult Index()
